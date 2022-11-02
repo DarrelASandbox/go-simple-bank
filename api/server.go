656df2586df0b60c6ccff619d7d92e10e27c8e2d
DarrelA <darrelaiscoding@gmail.com>
2022-11-02 10:04:12 +0800

    server.getAccount & server.listAccounts

--- api/server.go
+++ api/server.go
@@ -16,7 +16,9 @@ func NewServer(store *db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 
-	router.POST("/accounts", server.createAccount)
+	router.POST("/account", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccounts)
 
 	server.router = router
 	return server
