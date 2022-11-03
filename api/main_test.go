e35d7737847a6b2a9f6ac23656f3a8731373829c
DarrelA <darrelaiscoding@gmail.com>
2022-11-03 21:59:30 +0800

    newTestServer that accepts config parameter

--- api/main_test.go
+++ api/main_test.go
@@ -3,9 +3,26 @@ package api
 import (
 	"os"
 	"testing"
+	"time"
+
+	db "github.com/DarrelASandbox/go-simple-bank/db/sqlc"
+	"github.com/DarrelASandbox/go-simple-bank/db/util"
+	"github.com/stretchr/testify/require"
 
 	"github.com/gin-gonic/gin"
 )
+
+func newTestServer(t *testing.T, store db.Store) *Server {
+	config := util.Config{
+		TokenSymmetricKey:   util.RandomString(32),
+		AccessTokenDuration: time.Minute,
+	}
+
+	server, err := NewServer(config, store)
+	require.NoError(t, err)
+
+	return server
+}
 
 func TestMain(m *testing.M) {
 	gin.SetMode(gin.TestMode)
