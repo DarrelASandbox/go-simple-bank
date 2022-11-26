5253e353d0a3794831cf5a31fe114f5c18c08944
DarrelA <darrelaiscoding@gmail.com>
2022-11-26 09:32:41 +0800

    custom interface `ResponseRecorder` using `ResponseWriter`

--- api_rpc/logger.go
+++ api_rpc/logger.go
@@ -42,20 +42,43 @@ func GrpcLogger(
 	return result, err
 }
 
+type ResponseRecorder struct {
+	http.ResponseWriter
+	StatusCode int
+	Body       []byte
+}
+
+func (rec *ResponseRecorder) WriteHeader(statusCode int) {
+	rec.StatusCode = statusCode
+	rec.ResponseWriter.WriteHeader(statusCode)
+}
+
+func (rec *ResponseRecorder) Write(body []byte) (int, error) {
+	rec.Body = body
+	return rec.ResponseWriter.Write(body)
+}
+
 func HttpLogger(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
 
 		startTime := time.Now()
-		handler.ServeHTTP(res, req)
+		rec := &ResponseRecorder{
+			ResponseWriter: res,
+			StatusCode:     http.StatusOK,
+		}
+		handler.ServeHTTP(rec, req)
 		duration := time.Since(startTime)
 
 		logger := log.Info()
+		if rec.StatusCode != http.StatusOK {
+			logger = log.Error().Bytes("body", rec.Body)
+		}
 
 		logger.Str("protocol", "http").
 			Str("method", req.Method).
 			Str("path", req.RequestURI).
-			// Int("status_code", int(statusCode)).
-			// Str("status_text", statusCode.String()).
+			Int("status_code", int(rec.StatusCode)).
+			Str("status_text", http.StatusText(rec.StatusCode)).
 			Dur("duration", duration).
 			Msg("received a HTTP request")
 	})
